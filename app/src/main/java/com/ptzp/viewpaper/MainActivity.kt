345a29ec8b7ea22dc2361b83e8b0d8979b1aec73
PyaeThinzarPhyo <50790283+PyaeThinzarPhyo@users.noreply.github.com>
2020-03-13 13:24:40 +0630

    First commit

---
+++ app/src/main/java/com/ptzp/viewpaper/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.ptzp.viewpaper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.viewpager.widget.ViewPager
+import com.ptzp.viewpaper.adapter.MoviePaperAdapter
+import com.viewpagerindicator.CirclePageIndicator
+import java.util.*
+import kotlin.collections.ArrayList
+
+class MainActivity : AppCompatActivity() {
+    private var imageModelArrayList: ArrayList<ViewPaperFragment>? = null
+
+    private val myImageList = intArrayOf(R.drawable.chair, R.drawable.chairone, R.drawable.chairtwo)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageModelArrayList = ArrayList()
+        imageModelArrayList = populateList()
+
+        init()
+
+    }
+
+    private fun init() {
+        mPager = findViewById(R.id.viewPaper) as ViewPager
+        mPager!!.adapter = MoviePaperAdapter(this@MainActivity, this.imageModelArrayList!!)
+
+        val indicator = findViewById(R.id.indicator) as CirclePageIndicator
+
+        indicator.setViewPager(mPager)
+
+        val density = resources.displayMetrics.density
+
+        //Set circle indicator radius
+        indicator.setRadius(5 * density)
+
+        NUM_PAGES = imageModelArrayList!!.size
+
+        // Auto start of viewpager
+        val handler = Handler()
+        val Update = Runnable {
+            if (currentPage == NUM_PAGES) {
+                currentPage = 0
+            }
+            mPager!!.setCurrentItem(currentPage++, true)
+        }
+        val swipeTimer = Timer()
+        swipeTimer.schedule(object : TimerTask() {
+            override fun run() {
+                handler.post(Update)
+            }
+        }, 2000, 2000)
+
+        // Pager listener over indicator
+        indicator.setOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+            override fun onPageSelected(position: Int) {
+                currentPage = position
+
+            }
+
+            override fun onPageScrolled(pos: Int, arg1: Float, arg2: Int) {
+
+            }
+
+            override fun onPageScrollStateChanged(pos: Int) {
+
+            }
+        })
+
+    }
+
+    companion object {
+
+        private var mPager: ViewPager? = null
+        private var currentPage = 0
+        private var NUM_PAGES = 0
+    }
+
+
+    private fun populateList(): ArrayList<ViewPaperFragment> {
+
+        val list = ArrayList<ViewPaperFragment>()
+
+        for (i in 0..2) {
+            val viewPaper = ViewPaperFragment()
+            viewPaper.setImage_drawables(myImageList[i])
+            list.add(viewPaper)
+        }
+
+        return list
+    }
+
+}
